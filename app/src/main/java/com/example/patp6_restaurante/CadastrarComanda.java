0df699832db3fdf9476d309fd8b2a643baa43888
Matheus Leal <s.matheus.l@gmail.com>
2022-10-13 13:35:04 -0300

    producao 13-10

---
+++ app/src/main/java/com/example/patp6_restaurante/CadastrarComanda.java
@@ -0,0 +1,14 @@
+package com.example.patp6_restaurante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarComanda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_comanda);
+    }
+}
--- app/src/main/java/com/example/patp6_restaurante/Comandas.java
+++ app/src/main/java/com/example/patp6_restaurante/Comandas.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ActivityComandas extends AppCompatActivity {
+public class Comandas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
