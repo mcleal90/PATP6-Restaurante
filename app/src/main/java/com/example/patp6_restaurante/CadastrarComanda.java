837736b618e05f2ffe2efc006f0cbf616f48c08d
TETEU <s.matheus.l@gmail.com>
2022-10-19 16:31:32 -0300

    producao 19102022

--- app/src/main/java/com/example/patp6_restaurante/CadastrarComanda.java
+++ app/src/main/java/com/example/patp6_restaurante/CadastrarComanda.java
@@ -2,18 +2,33 @@ package com.example.patp6_restaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastrarComanda extends AppCompatActivity {
-    Button btCadComandaVoltar;
+    EditText edComanda,edNomeCliente,edTelefoneCliente,edMesa;
+    Button btCadComandaVoltar,btCadComandaSalvar;
+    SQLiteDatabase bancoDados;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_comanda);
+
 //identificacao de elementos
+        edComanda = findViewById(R.id.edComanda);
+        edNomeCliente = findViewById(R.id.edNomeCliente);
+        edTelefoneCliente = findViewById(R.id.edTelefoneCliente);
+        edMesa = findViewById(R.id.edMesa);
+        btCadComandaSalvar=findViewById(R.id.btCadComandaSalvar);
         btCadComandaVoltar=findViewById(R.id.btCadComandaVoltar);
+//Banco de dados
+        bancoDados = openOrCreateDatabase("bdRestaurante",MODE_PRIVATE,null);
 //botao voltar para o menu
         btCadComandaVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +36,51 @@ public class CadastrarComanda extends AppCompatActivity {
                 finish();
             }
         });
+//botao salvar
+        btCadComandaSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SalvaComanda();
+            }
+        });
+    }
 
+    //funcao caixa de texto
+    private void mostraCxTexto(String msg, String titulo) {
+//Gera uma caixa de texto na tela com um bot�o
+        //"OK"
+        AlertDialog.Builder builder = new
+                AlertDialog.Builder(this);
+        builder.setMessage(msg);
+        builder.setNeutralButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.setTitle(titulo);
+        dialog.show();
+    }
+
+    private void SalvaComanda() {
+        //Salva cadastro da comanda
+        try {
+            bancoDados.execSQL("INSERT INTO pedido(numero_com," +
+                    "cliente," +
+                    "telefone," +
+                    "mesa) " +
+                    "VALUES ('"+
+                    edComanda.getText().toString() + "','" +
+                    edNomeCliente.getText().toString() + "','" +
+                    edTelefoneCliente.getText().toString() + "','" +
+                    edMesa.getText().toString() + "')");
+
+            btCadComandaSalvar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent abrircomandas = new Intent(getApplicationContext(),Comandas.class);
+                    startActivity(abrircomandas);
+                }
+            });
+        } catch (Exception e) {
+            mostraCxTexto("Inserindo BD. Mensagem: " +
+                    e.getMessage(), "ERRO");
+        }
     }
 }
--- app/src/main/java/com/example/patp6_restaurante/Login.java
+++ app/src/main/java/com/example/patp6_restaurante/Login.java
@@ -2,33 +2,65 @@ package com.example.patp6_restaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-    EditText edUser, edSenha;
-    Button btLogin;
+        EditText edUser, edSenha;
+        Button btLogin;
+        private SQLiteDatabase bancoDados;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
 
-        edUser = findViewById(R.id.edUser);
-        edSenha = findViewById(R.id.edSenha);
-        btLogin = findViewById(R.id.btLogin);
-//botao logar - abrir menu mas falta verificar autenticação
-        btLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                setContentView(R.layout.activity_menu);
-                Intent logarmenu = new Intent(getApplicationContext(),Menu.class);
-                startActivity(logarmenu);
-            }
-        });
+//identifica elementos
+            edUser = findViewById(R.id.edUser);
+            edSenha = findViewById(R.id.edSenha);
+            btLogin = findViewById(R.id.btLogin);
 
+//funcao criar o banco de dados
+            criarBancoDados();
+
+//clique botao logar - abrir menu mas falta verificar autenticação
+            btLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent logarmenu = new Intent(getApplicationContext(),CadastrarComanda.class);
+                    startActivity(logarmenu);
+                }
+            });
+        }
+
+//funcao caixa de texto
+    private void mostraCxTexto(String msg, String titulo) {
+//Gera uma caixa de texto na tela com um bot�o
+        //"OK"
+        AlertDialog.Builder builder = new
+                AlertDialog.Builder(this);
+        builder.setMessage(msg);
+        builder.setNeutralButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.setTitle(titulo);
+        dialog.show();
     }
-}
+
+    private void criarBancoDados() {
+            try {
+                bancoDados = openOrCreateDatabase("bdRestaurante",MODE_PRIVATE,null);
+                bancoDados.execSQL("CREATE TABLE IF NOT EXISTS login(idlogin INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR, senha VARCHAR)");
+                bancoDados.execSQL("CREATE TABLE IF NOT EXISTS comanda(numero INTEGER PRIMARY KEY, situacao INTEGER)");
+                bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pedido(idpedido INTEGER PRIMARY KEY AUTOINCREMENT, numero_com INTEGER, mesa INTEGER, cliente VARCHAR, telefone VARCHAR)");
+                bancoDados.execSQL("CREATE TABLE IF NOT EXISTS cardapio(idcardapio INTEGER PRIMARY KEY AUTOINCREMENT, descricao VARCHAR, valor VARCHAR)");
+                bancoDados.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
