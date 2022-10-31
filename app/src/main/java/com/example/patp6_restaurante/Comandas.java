64debf716d85efd81d2d8b05f3488a5e74ea3a4e
Matheus Leal <s.matheus.l@gmail.com>
2022-10-31 13:19:02 -0300

    producao 31-10

--- app/src/main/java/com/example/patp6_restaurante/Comandas.java
+++ app/src/main/java/com/example/patp6_restaurante/Comandas.java
@@ -2,34 +2,68 @@ package com.example.patp6_restaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Comandas extends AppCompatActivity {
     EditText edComandas,EdItemCardapio,edQuantidade;
-    Button btAdicionarPedido,btComandasVoltar,btComandasVer,btFecharPedido;
+    Button btAdicionarPedido, btNovaComanda,btComandasVer,btFecharPedido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comandas);
 
-        btComandasVoltar = findViewById(R.id.btComandasVoltar);
+        btNovaComanda = findViewById(R.id.btNovaComanda);
         btComandasVer = findViewById(R.id.btComandasVer);
+        btFecharPedido = findViewById(R.id.btFecharPedido);
 
-//botao voltar para o menu
-        btComandasVoltar.setOnClickListener(new View.OnClickListener() {
+
+//botao adicionar pedido
+        btAdicionarPedido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                adicionarPedido();
             }
         });
 
+//botao nova comanda
+        btNovaComanda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nova = new Intent(getApplicationContext(),CadastrarComanda.class);
+                startActivity(nova);
+            }
+        });
+
+//botao fechar pedido
+    btFecharPedido.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            fecharPedido();
+        }
+    });
+
+//botao ver pedidos da comanda
+    btComandasVer.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent ver = new Intent(getApplicationContext(),Pedidos.class);
+            startActivity(ver);
+        }
+    });
+    }
+
+//funções
+
+    private void fecharPedido() {
+        //altera a situação da comanda (1=livre,2=uso,3=fechada)
+    }
+
+    private void adicionarPedido() {
+        //adiciona à tabela pedido as informações: idcardápio_ped = edItemCardapio + quantidade = edQuantidade ONDE idcliente_ped = edComandas
     }
 }
